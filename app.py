6774b482e6281bdb7e4b56e75d349d04ba426428
XeroSWM <wxmonteros@uce.edu.ec>
2025-05-26 07:09:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"<h1>hola Mundo</h1>" \
+    return f"<h1>funciono?</h1>" \
            f"<p>Última actualización: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}</p>"
 
 if __name__ == "__main__":
